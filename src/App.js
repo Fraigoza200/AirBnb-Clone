0c60d12771101486ef293a4678e8e88d094454aa
Francisco Raigoza <raigoza.frankie@gmail.com>
2020-10-20 14:32:40 -0700

    Home and Header created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
+import Home from './Home'
+import Header from './Header'
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
      <h1>AirBnB-Clone</h1>
+    <Header />
+    <Home />
+     {/* Header */}
+
+     {/* Banner */}
+
+     {/* Cards */}
+
+     {/* Footer */}
+
+     {/* SearchPage */}
     </div>
   );
 }
