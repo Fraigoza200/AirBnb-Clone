390afcf4fe87e137bf2dac5e7484157f2e751e6b
Francisco Raigoza <raigoza.frankie@gmail.com>
2020-10-22 16:47:46 -0700

    created Banner with image

---
+++ src/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+    return (
+        <div className='banner'>
+            <div className='banner__info'>
+
+        </div>
+    )
+}
+
+export default Banner
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import './Home.css'
+import Banner from './Banner'
 
 function Home() {
     return (
         <div className='home'>
+            <h1>Home Component</h1>
+            <Banner />
         </div>
     )
 }
