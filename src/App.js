872ff9326c9e616304cfc76a9d217a4b4fc49690
Francisco Raigoza <raigoza.frankie@gmail.com>
2020-10-23 15:24:35 -0700

    Card finished, Footer started

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Home from './Home'
 import Header from './Header'
+import Footer from './Footer'
 
 function App() {
   return (
     <div className="app">
     <Header />
     <Home />
+    <Footer />
      {/* Header */}
 
      {/* Banner */}
--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Card.css'
 
-function Card() {
+function Card({ src, title, description, price }) {
     return (
-        <div>
-            
+        <div className='card'>
+            <img src={src} alt='image' />
+            <div className='card__info'>
+                <h2>{title}</h2>
+                <h4>{description}</h4>
+                <h3>{price}</h3>
+            </div>
         </div>
     )
 }
