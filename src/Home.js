2847b0066b7ae40339df276502fb1d369e8fa354
Francisco Raigoza <raigoza.frankie@gmail.com>
2020-10-23 15:02:19 -0700

    created card components to store images

--- src/Home.js
+++ src/Home.js
@@ -6,6 +6,18 @@ function Home() {
     return (
         <div className='home'>
             <Banner />
+
+            <div className='home__section'>
+                <Card />
+                <Card />
+                <Card />
+            </div>
+            
+            <div className='home__section'>
+                <Card />
+                <Card />
+                <Card />
+            </div>
         </div>
     )
 }
