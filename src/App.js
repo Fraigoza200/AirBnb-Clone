f104da4b5f86469e2f99271a305ff8db3f9742b6
Francisco Raigoza <raigoza.frankie@gmail.com>
2020-10-23 15:37:17 -0700

    created SearchPage for React Routing

--- src/App.js
+++ src/App.js
@@ -3,13 +3,30 @@ import './App.css';
 import Home from './Home'
 import Header from './Header'
 import Footer from './Footer'
+import SearchPage from './SearchPage'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="app">
-    <Header />
-    <Home />
-    <Footer />
+      <Router>
+        <Header />
+
+      <Switch>
+        
+        <Route path='/search'>
+            <SearchPage />
+        </Route>
+
+        <Route path='/'>
+           <Home />
+        </Route>
+
+
+        </Switch>
+        <Footer />
+
+      </Router>
     </div>
   );
 }
---
+++ src/SearchPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SearchPage.css'
+
+function SearchPage() {
+    return (
+        <div>
+            <h1>SEARCH PAGE</h1>
+        </div>
+    )
+}
+
+export default SearchPage
