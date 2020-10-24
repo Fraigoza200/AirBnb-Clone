6dc43648a0fbc82fecf35877ed660981e1e1efa4
Francisco Raigoza <raigoza.frankie@gmail.com>
2020-10-23 17:18:56 -0700

    created route to Search Page and created Result

---
+++ src/SearchResult.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SearchResult.css'
+
+function SearchResult() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SearchResult
